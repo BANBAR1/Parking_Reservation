9a45ac18e1abfbdd64050b0e89f42484f11bb112
BANBAR <rkorovechkiy@gmail.com>
2026-08-22 16:56:31 +0200

    Some fixes + .gitignore changes

--- Buildings/building.py
+++ Buildings/building.py
@@ -1,23 +1,23 @@
-from Parking_Lots.parking_lot import Parking_lot
+from Parking_Lots.parkinglot import ParkingLot
 
-class Building_builder():
+class BuildingBuilder:
 
     def __init__(self, normal_parking_space_amount, special_parking_space_amount = 0):
 
-        self.parking_lot = Parking_lot(normal_parking_space_amount, special_parking_space_amount)
+        self.parking_lot = ParkingLot(normal_parking_space_amount, special_parking_space_amount)
 
     @property
     def general_amount_of_parking_spaces(self):
 
-        return self.parking_lot.get_parking_space_amount()
+        return self.parking_lot.parking_space_amount
 
     @property
     def amount_of_normal_parking_spaces(self):
 
-        return self.parking_lot.get_normal_parking_space_amount()
+        return self.parking_lot.normal_parking_space_amount
 
     @property
     def amount_of_special_parking_spaces(self):
     
-        return self.parking_lot.get_special_parking_space_amount()
+        return self.parking_lot.special_parking_space_amount
     
